5555e5f98f897c50f879f1c302d000982b65c411
VictorDS27 <victor.vieira2704@gmail.com>
2022-05-11 14:07:57 -0300

    exluir

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -48,4 +48,9 @@ class Home extends BaseController
 
         return redirect('pessoa');
     }
+    public function excluir($id = null){
+        $model = new PessoasModel();
+        $model->delete($id);
+        return redirect("pessoa");
+    }
 }
--- app/Views/pessoa.php
+++ app/Views/pessoa.php
@@ -5,6 +5,7 @@
     <td>Nome</td>
     <td>Profissão</td>
     <td>Idade</td>
+    <td></td>
  </tr> 
 
  <?php
@@ -16,6 +17,8 @@
    <td> <?= $pessoa_item['nome'] ?></td> 
    <td> <?= $pessoa_item['profissao'] ?></td> 
    <td> <?= $pessoa_item['idade'] ?></td> 
+   <td><a href="excluir/<?=$pessoa_item['id']?>" class="btn btn-danger">Apagar</a></td>
+   <td><a href="" class="btn btn-warning">Editar</a></td>
   </tr>
 </div>
 
