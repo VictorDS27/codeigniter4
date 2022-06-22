0211654155ffae13a42c6ad8f6e06464b7a72d27
VictorDS27 <victor.vieira2704@gmail.com>
2022-06-22 14:03:32 -0300

    ata

--- app/Views/temp/header.php
+++ app/Views/temp/header.php
@@ -38,20 +38,63 @@
   <path d="M7.001 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0z"/>
 </svg> Quem Somos</a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="contato">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
 </svg> Contato</a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="login">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-door-open" viewBox="0 1 16 16">
   <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
   <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
 </svg>
           Login</a>
+
         </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link" href="cadastro-pessoas">
+            
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 1 16 16">
+          <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
+          <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 1z"/>
+          </svg>
+
+          Cadastrar</a>
+        </li>
+
+
+        <?php
+
+                                $session = \Config\Services::session();
+
+                                if($session->get('logado')){
+
+                            ?>
+        <li class="nav-item">
+          <a class="nav-link" href="cadastro-carro">
+            
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+         </svg>
+
+          Cadastrar Carro</a>
+        </li>
+                        
+
+                     <?php
+
+                                }
+
+                            //fim if
+
+                            ?>
       </ul>
     </div>
   </div>
